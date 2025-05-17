f9ef7922275cf00fb287b516212515a58dca3510
mataa019 <johnnymataa019@gmail.com>
2025-05-17 12:39:03 +0200

    implementing the Employee Api

--- app/src/main/java/com/example/cpringclient/retrofitservice/EmployeeApi.java
+++ app/src/main/java/com/example/cpringclient/retrofitservice/EmployeeApi.java
@@ -1,4 +1,13 @@
 package com.example.cpringclient.retrofitservice;
 
+import java.util.List;
+
+import retrofit2.http.GET;
+
 public class EmployeeApi {
+    @GET("/employee/get-all")
+    Call<List<Employee>> getAllEmployee();
+
+    @GET("/employee/save")
+    Call<List<Employee>> SaveEmployee();
 }
