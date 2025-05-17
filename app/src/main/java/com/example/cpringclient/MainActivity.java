ce11c93183fe2de209cea4c0245c5478d81e6a0b
mataa019 <johnnymataa019@gmail.com>
2025-05-17 15:05:05 +0200

    implementing the Employee Api

--- app/src/main/java/com/example/cpringclient/MainActivity.java
+++ app/src/main/java/com/example/cpringclient/MainActivity.java
@@ -21,6 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Name = findViewById()
+        //Name = findViewById()
     }
 }
