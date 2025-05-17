0c687ef064e40a89ce43053bf11e89836cbac617
mataa019 <johnnymataa019@gmail.com>
2025-05-17 12:31:23 +0200

    implementing the Retrofit

--- app/src/main/java/com/example/cpringclient/retrofitservice/RetrofitService.java
+++ app/src/main/java/com/example/cpringclient/retrofitservice/RetrofitService.java
@@ -1,6 +1,9 @@
 package com.example.cpringclient.retrofitservice;
 
+import com.google.gson.Gson;
+
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitService {
     private Retrofit retrofit;
@@ -9,6 +12,8 @@ public class RetrofitService {
        initializedRetrofit();
     }
   public  void initializedRetrofit(){
-
+        retrofit = new Retrofit.Builder()
+                .baseUrl("hppt://192.168.1.50:8080")
+                .addConverterFactory(GsonConverterFactory(new Gson()))
   }
 }
